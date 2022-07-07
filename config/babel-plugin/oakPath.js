31b91484aaab00ac0e06cceefec7b992e37b80f7
Xc@centOs <chinawraith@163.com>
2022-07-07 14:46:14 +0800

    编译过程中对于render和import的注入

--- config/babel-plugin/oakPath.js
+++ config/babel-plugin/oakPath.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const { relative } = require('path');
 const t = require('@babel/types');
 const pull = require('lodash/pull');
 const { assert } = require('console');
@@ -13,112 +15,137 @@ function isOakNamespaceIdentifier(node, name) {
 module.exports = (babel) => {
     return {
         visitor: {
+            Program(path, state) {
+                const { cwd, filename } = state;
+                const rel = relative(cwd, filename).replace(/\\/g, '/');
+                if (/(pages|components)[\w|\W]+(index\.tsx|index\.pc\.tsx)$/.test(rel)) {
+                    const lessFile = filename.replace(/\.ts$/, '.less');
+                    const lessFileExists = fs.existsSync(lessFile);
+                    const pcLessFile = filename.replace(/\.ts$/, '.pc.less');
+                    const pcLessFileExists = fs.existsSync(pcLessFile);
+
+                    const { body } = path.node;
+                    const lessFileImport = rel.endsWith('.pc.tsx') ?
+                        (pcLessFileExists ? './index.pc.less' : './index.less') :
+                        (lessFileExists ? './index.less' : './index.pc.less');
+                    body.unshift(
+                        t.importDeclaration(
+                            [],
+                            t.stringLiteral(lessFileImport)
+                        )
+                    );
+                }
+            },
             JSXAttribute(path, state) {
-                const node = path.node;
-
-                if (isOakNamespaceIdentifier(node.name, 'path')) {
-                    // 若存在oak:path，则注入oakParent={this.state.oakFullpath}和oakPath={oak:path}
-                    assert(t.isJSXOpeningElement(path.parent));
-                    const { attributes } = path.parent;
-
-                    const parentAttr = attributes.find(
-                        (ele) =>
-                            t.isJSXIdentifier(ele.name) &&
-                            ele.name.name === 'oakParent'
-                    );
-                    if (parentAttr) {
-                        console.warn(
-                            `「${state.filename}」有JSX元素同时定义了oak:path和oakParent，请确保oakParent等于{this.state.oakFullpath}`
+                const { cwd, filename } = state;
+                const rel = relative(cwd, filename).replace(/\\/g, '/');
+                if (/(pages|components)[\w|\W]+(index\.tsx|index\.pc\.tsx)$/.test(rel))  {
+                    const { node } = path;
+                    if (isOakNamespaceIdentifier(node.name, 'path')) {
+                        // 若存在oak:path，则注入oakParent={this.state.oakFullpath}和oakPath={oak:path}
+                        assert(t.isJSXOpeningElement(path.parent));
+                        const { attributes } = path.parent;
+    
+                        const parentAttr = attributes.find(
+                            (ele) =>
+                                t.isJSXIdentifier(ele.name) &&
+                                ele.name.name === 'oakParent'
                         );
-                    } else {
-                        attributes.push(
-                            t.jsxAttribute(
-                                t.jsxIdentifier('oakParent'),
-                                t.jsxExpressionContainer(
-                                    t.memberExpression(
+                        if (parentAttr) {
+                            console.warn(
+                                `「${state.filename}」有JSX元素同时定义了oak:path和oakParent，请确保oakParent等于{this.state.oakFullpath}`
+                            );
+                        } else {
+                            attributes.push(
+                                t.jsxAttribute(
+                                    t.jsxIdentifier('oakParent'),
+                                    t.jsxExpressionContainer(
                                         t.memberExpression(
-                                            t.thisExpression(),
-                                            t.identifier('state')
-                                        ),
-                                        t.identifier('oakFullpath')
+                                            t.memberExpression(
+                                                t.thisExpression(),
+                                                t.identifier('state')
+                                            ),
+                                            t.identifier('oakFullpath')
+                                        )
                                     )
                                 )
-                            )
+                            );
+                        }
+    
+                        const pathAttr = attributes.find(
+                            (ele) =>
+                                t.isJSXIdentifier(ele.name) &&
+                                ele.name.name === 'oakPath'
                         );
+    
+                        if (pathAttr) {
+                            console.warn(
+                                `「${state.filename}」有JSX元素同时定义了oak:path和oakPath，请确保两者相等`
+                            );
+                        } else {
+                            attributes.push(
+                                t.jsxAttribute(
+                                    t.jsxIdentifier('oakPath'),
+                                    node.value
+                                )
+                            );
+                        }
+                        path.remove();
                     }
-
-                    const pathAttr = attributes.find(
-                        (ele) =>
-                            t.isJSXIdentifier(ele.name) &&
-                            ele.name.name === 'oakPath'
-                    );
-
-                    if (pathAttr) {
-                        console.warn(
-                            `「${state.filename}」有JSX元素同时定义了oak:path和oakPath，请确保两者相等`
+                    else if (isOakNamespaceIdentifier(node.name, 'value')) {
+                        // 如果是oak:value，增加value和data-attr属性
+                        assert(t.isJSXOpeningElement(path.parent));
+                        assert(t.isStringLiteral(node.value));
+                        const { attributes } = path.parent;
+    
+                        const valueAttr = attributes.find(
+                            (ele) =>
+                                t.isJSXIdentifier(ele.name) &&
+                                ele.name.name === 'value'
                         );
-                    } else {
-                        attributes.push(
-                            t.jsxAttribute(
-                                t.jsxIdentifier('oakPath'),
-                                node.value
-                            )
-                        );
-                    }
-                    path.remove();
-                } else if (isOakNamespaceIdentifier(node.name, 'value')) {
-                    // 如果是oak:value，增加value和data-attr属性
-                    assert(t.isJSXOpeningElement(path.parent));
-                    assert(t.isStringLiteral(node.value));
-                    const { attributes } = path.parent;
-
-                    const valueAttr = attributes.find(
-                        (ele) =>
-                            t.isJSXIdentifier(ele.name) &&
-                            ele.name.name === 'value'
-                    );
-                    if (valueAttr) {
-                        console.warn(
-                            `「${state.filename}」有JSX元素同时定义了oak:value和value，请确保value等于{this.state["oak:value"]}`
-                        );
-                    } else {
-                        attributes.push(
-                            t.jsxAttribute(
-                                t.jsxIdentifier('value'),
-                                t.jsxExpressionContainer(
-                                    t.memberExpression(
+                        if (valueAttr) {
+                            console.warn(
+                                `「${state.filename}」有JSX元素同时定义了oak:value和value，请确保value等于{this.state["oak:value"]}`
+                            );
+                        } else {
+                            attributes.push(
+                                t.jsxAttribute(
+                                    t.jsxIdentifier('value'),
+                                    t.jsxExpressionContainer(
                                         t.memberExpression(
-                                            t.thisExpression(),
-                                            t.identifier('state')
-                                        ),
-                                        t.identifier(node.value.value)
+                                            t.memberExpression(
+                                                t.thisExpression(),
+                                                t.identifier('state')
+                                            ),
+                                            t.identifier(node.value.value)
+                                        )
                                     )
                                 )
-                            )
+                            );
+                        }
+    
+                        const dataAttrAttr = attributes.find(
+                            (ele) =>
+                                t.isJSXIdentifier(ele.name) &&
+                                ele.name.name === 'data-attr'
                         );
+    
+                        if (dataAttrAttr) {
+                            assert(
+                                t.isStringLiteral(dataAttrAttr.value) &&
+                                    dataAttrAttr.value.value === node.value.value,
+                                `「${state.filename}」中有JSX元素同时定义了oak:value和data-attr，且两者的值不相等`
+                            );
+                        } else {
+                            attributes.push(
+                                t.jsxAttribute(
+                                    t.jsxIdentifier('data-attr'),
+                                    node.value
+                                )
+                            );
+                        }
+                        path.remove();
                     }
-
-                    const dataAttrAttr = attributes.find(
-                        (ele) =>
-                            t.isJSXIdentifier(ele.name) &&
-                            ele.name.name === 'data-attr'
-                    );
-
-                    if (dataAttrAttr) {
-                        assert(
-                            t.isStringLiteral(dataAttrAttr.value) &&
-                                dataAttrAttr.value.value === node.value.value,
-                            `「${state.filename}」中有JSX元素同时定义了oak:value和data-attr，且两者的值不相等`
-                        );
-                    } else {
-                        attributes.push(
-                            t.jsxAttribute(
-                                t.jsxIdentifier('data-attr'),
-                                node.value
-                            )
-                        );
-                    }
-                    path.remove();
                 }
             },
         },
--- config/babel-plugin/oakRender.js
+++ config/babel-plugin/oakRender.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const { relative } = require('path');
 const t = require('@babel/types');
 const pull = require('lodash/pull');
 const { assert } = require('console');
@@ -6,36 +8,131 @@ module.exports = (babel) => {
     return {
         visitor: {
             Program(path, state) {
-                const node = path.node;
-                const body = node.body;
-                let isOak = false;
-                body.forEach((node2) => {
-                    if (
-                        node2 &&
-                        node2.declaration &&
-                        node2.declaration.callee &&
-                        (node2.declaration.callee.name === 'OakPage' ||
-                            node2.declaration.callee.name === 'OakComponent')
-                    ) {
-                        isOak = true;
-                        node2.declaration.arguments.forEach((node3) => {
-                            if (t.isObjectExpression(node3)) {
-                                const propertyRender = t.objectProperty(
+                const { cwd, filename } = state;
+                const rel = relative(cwd, filename).replace(/\\/g, '/');
+                if (/pages|components[\w|\W]+index\.ts$/.test(rel)) {
+                    const tsxFile = filename.replace(/\.ts$/, '.tsx');
+                    const tsxFileExists = fs.existsSync(tsxFile);
+                    const pcTsxFile = filename.replace(/\.ts$/, '.pc.tsx');
+                    const pcTsxFileExists = fs.existsSync(pcTsxFile);
+                    /** 根据tsx文件存在的情况，注入如下的render代码
+                     * if (true) {
+                            const renderMobile = require('./index.tsx').default;
+                            return renderMobile.call(this);
+                        }
+                        else {
+                            const renderScreen = require('./index.screen.tsx').default;
+                            return renderScreen.call(this);
+                        }
+                     */
+                    const renderStatements = [
+                        t.variableDeclaration('const', [
+                            t.variableDeclarator(
+                                t.identifier('render'),
+                                t.memberExpression(
+                                    t.callExpression(
+                                        t.identifier('require'),
+                                        [
+                                            t.stringLiteral('./index.tsx')
+                                        ]
+                                    ),
+                                    t.identifier('default')
+                                )
+                            )
+                        ]),
+                        t.returnStatement(
+                            t.callExpression(
+                                t.memberExpression(
                                     t.identifier('render'),
-                                    t.identifier('render')
-                                );
-                                node3.properties.unshift(propertyRender);
-                            }
-                        });
+                                    t.identifier('call')
+                                ),
+                                [
+                                    t.thisExpression(),
+                                ]
+                            )
+                        )
+                    ];
+                    const renderPcStatements = [
+                        t.variableDeclaration('const', [
+                            t.variableDeclarator(
+                                t.identifier('render'),
+                                t.memberExpression(
+                                    t.callExpression(
+                                        t.identifier('require'),
+                                        [
+                                            t.stringLiteral('./index.pc.tsx')
+                                        ]
+                                    ),
+                                    t.identifier('default')
+                                )
+                            )
+                        ]),
+                        t.returnStatement(
+                            t.callExpression(
+                                t.memberExpression(
+                                    t.identifier('render'),
+                                    t.identifier('call')
+                                ),
+                                [
+                                    t.thisExpression(),
+                                ]
+                            )
+                        )
+                    ];
+                    const statements = [];
+                    if (tsxFileExists && pcTsxFileExists) {
+                        statements.push(
+                            t.ifStatement(
+                                t.booleanLiteral(true),
+                                t.blockStatement(
+                                    renderStatements
+                                ),
+                                t.blockStatement(
+                                    renderPcStatements
+                                )
+                            )
+                        );
                     }
-                });
-                if (isOak) {
-                    const importRender = t.importDeclaration(
-                        [t.importDefaultSpecifier(t.identifier('render'))],
-                        t.stringLiteral('./index.tsx')
-                    );
-
-                    body.unshift(importRender);
+                    else if (tsxFileExists) {
+                        statements.push(
+                            ...renderStatements
+                        );
+                    }
+                    else if (pcTsxFileExists) {
+                        statements.push(
+                            ...renderPcStatements
+                        );
+                    }
+                    else {
+                        assert(false, `${filename}文件夹中不存在index.tsx或者index.pc.tsx`);
+                    }
+                    const node = path.node;
+                    const body = node.body;
+                    body.forEach((node2) => {
+                        if (
+                            node2 &&
+                            node2.declaration &&
+                            node2.declaration.callee &&
+                            (node2.declaration.callee.name === 'OakPage' ||
+                                node2.declaration.callee.name === 'OakComponent')
+                        ) {
+                            node2.declaration.arguments.forEach((node3) => {
+                                if (t.isObjectExpression(node3)) {
+                                    const propertyRender = t.objectProperty(
+                                        t.identifier('render'),
+                                        t.functionExpression(
+                                            null,
+                                            [],
+                                            t.blockStatement(
+                                                statements
+                                            )
+                                        )
+                                    );
+                                    node3.properties.unshift(propertyRender);
+                                }
+                            });
+                        }
+                    });                    
                 }
             },
         },
