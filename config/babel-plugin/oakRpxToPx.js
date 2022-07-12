bb98bf39a854496d528b33b2c84b2df0bfbc9c83
wkj <278599135@qq.com>
2022-07-12 11:39:06 +0800

    postcss 8格式

--- config/babel-plugin/oakRpxToPx.js
+++ config/babel-plugin/oakRpxToPx.js
@@ -2,12 +2,30 @@
 const postcss = require('postcss');
 const Rpx2px = require('./rpx2px');
 
-module.exports = postcss.plugin('postcss-rpx2px', function (options) {
-    return function (css, result) {
-        const oldCssText = css.toString();
-        const rpx2pxIns = new Rpx2px(options);
-        const newCssText = rpx2pxIns.generatePx(oldCssText);
-        const newCssObj = postcss.parse(newCssText);
-        result.root = newCssObj;
+// module.exports = postcss.plugin('postcss-rpx2px', function (options) {
+//     return function (css, result) {
+//         const oldCssText = css.toString();
+//         const rpx2pxIns = new Rpx2px(options);
+//         const newCssText = rpx2pxIns.generatePx(oldCssText);
+//         const newCssObj = postcss.parse(newCssText);
+//         result.root = newCssObj;
+//     };
+// });
+
+module.exports = function (options) {
+    return {
+        postcssPlugin: 'postcss-rpx2px',
+        Once: function (css, { result, AtRule }) {
+            const oldCssText = css.toString();
+            const rpx2pxIns = new Rpx2px(options);
+            const newCssText = rpx2pxIns.generatePx(oldCssText);
+            const newCssObj = postcss.parse(newCssText);
+            result.root = newCssObj;
+        },
+        // Declaration(decl) {
+        //     console.log(decl.toString());
+        // },
     };
-});
+};
+
+module.exports.postcss = true;
