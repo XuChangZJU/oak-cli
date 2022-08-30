be509b0983bd665e7d1cdf6504706848b2214daf
wkj <278599135@qq.com>
2022-08-30 19:42:48 +0800

    MakeOakComponent、MakeOakPage 写出一个声明

--- template/src/typings/polyfill.d.ts
+++ template/src/typings/polyfill.d.ts
@@ -1,9 +1,8 @@
 import { GenerateIdOption } from '../utils/polyfill';
-import { MakeOakComponent, MakeOakPage } from 'oak-frontend-base/lib/page.mp';
 import {
-    MakeOakComponent as MakeOakWebComponent,
-    MakeOakPage as MakeOakWebPage,
-} from 'oak-frontend-base/lib/page.web';
+    MakeOakComponent,
+    MakeOakPage,
+} from 'oak-frontend-base/lib/types/Page';
 import { EntityDict } from 'oak-app-domain';
 import { RuntimeContext } from '../RuntimeContext';
 import { aspectDict } from '../aspects';
@@ -13,31 +12,17 @@ declare global {
     const __DEV__: boolean;
     const generateNewId: (option?: GenerateIdOption) => Promise<string>;
     const getRandomValues: (length: number) => Promise<Uint8Array>;
-    const OakPage:
-        | MakeOakPage<
-              EntityDict,
-              RuntimeContext,
-              typeof aspectDict,
-              ReturnType<typeof initialize>['features']
-          >
-        | MakeOakWebPage<
-              EntityDict,
-              RuntimeContext,
-              typeof aspectDict,
-              ReturnType<typeof initialize>['features']
-          >;
-    const OakComponent:
-        | MakeOakComponent<
-              EntityDict,
-              RuntimeContext,
-              typeof aspectDict,
-              ReturnType<typeof initialize>['features']
-          >
-        | MakeOakWebComponent<
-              EntityDict,
-              RuntimeContext,
-              typeof aspectDict,
-              ReturnType<typeof initialize>['features']
-          >;
+    const OakPage: MakeOakPage<
+        EntityDict,
+        RuntimeContext,
+        typeof aspectDict,
+        ReturnType<typeof initialize>['features']
+    >;
+    const OakComponent: MakeOakComponent<
+        EntityDict,
+        RuntimeContext,
+        typeof aspectDict,
+        ReturnType<typeof initialize>['features']
+    >;
 }
 export {};
