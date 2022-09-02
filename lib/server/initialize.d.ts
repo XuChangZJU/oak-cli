9e8353b5ca8cf01eecdabc353185d40a77a9c17b
Xc@centOs <chinawraith@163.com>
2022-09-02 15:13:00 +0800

    声明中的小问题

--- lib/server/initialize.d.ts
+++ lib/server/initialize.d.ts
@@ -1,4 +1,5 @@
 /// <reference path="../../src/typings/polyfill.d.ts" />
 import { Context, EntityDict, RowStore } from 'oak-domain/lib/types';
+import { EntityDict as BaseEntityDict } from 'oak-domain/lib/base-app-domain';
 import { MySQLConfiguration } from 'oak-db/lib/MySQL/types/Configuration';
-export declare function initialize<ED extends EntityDict, Cxt extends Context<ED>>(path: string, contextBuilder: (scene?: string) => (store: RowStore<ED, Cxt>) => Cxt, dbConfig: MySQLConfiguration, dropIfExists?: boolean): Promise<void>;
+export declare function initialize<ED extends EntityDict & BaseEntityDict, Cxt extends Context<ED>>(path: string, contextBuilder: (scene?: string) => (store: RowStore<ED, Cxt>) => Cxt, dbConfig: MySQLConfiguration, dropIfExists?: boolean): Promise<void>;
