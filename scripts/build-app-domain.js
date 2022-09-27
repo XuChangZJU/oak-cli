9bb1e7c31f7f108f5a8db0cf31035028f21fa80c
Xc@centOs <chinawraith@163.com>
2022-09-27 13:13:41 +0800

    server相关的接口修改

--- scripts/build-app-domain.js
+++ scripts/build-app-domain.js
@@ -2,7 +2,7 @@
 const {
     buildSchema,
     analyzeEntities,
-} = require(`${process.cwd()}/node_modules/oak-domain/src/compiler/schemalBuilder`);
+} = require(`${process.cwd()}/node_modules/oak-domain/lib/compiler/schemalBuilder`);
 
 analyzeEntities(`${process.cwd()}/node_modules/oak-domain/src/entities`, 'oak-domain/lib/entities');
 analyzeEntities(`${process.cwd()}/node_modules/oak-general-business/src/entities`, 'oak-general-business/lib/entities');
--- src/server/initialize.ts
+++ src/server/initialize.ts
@@ -1,14 +1,14 @@
 /// <reference path="../typings/polyfill.d.ts" />
+import PathLib from 'path';
 import { AppLoader } from 'oak-backend-base';
 import { Context, EntityDict, RowStore } from 'oak-domain/lib/types';
 import { EntityDict as BaseEntityDict } from 'oak-domain/lib/base-app-domain';
-import { MySQLConfiguration } from 'oak-db/lib/MySQL/types/Configuration';
 
 export async function initialize<ED extends EntityDict & BaseEntityDict, Cxt extends Context<ED>>(
     path: string,
     contextBuilder: (scene?: string) => (store: RowStore<ED, Cxt>) => Promise<Cxt>,
-    dbConfig: MySQLConfiguration,
     dropIfExists?: boolean) {
+    const dbConfig = require(PathLib.join(path, '/configuration/mysql.json'));
     const appLoader = new AppLoader(path, contextBuilder, dbConfig);
     await appLoader.mount(true);
     await appLoader.initialize(dropIfExists);
