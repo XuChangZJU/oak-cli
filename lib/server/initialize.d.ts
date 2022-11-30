477fa1fe301165d4e7925467bb506d6617d649b3
Xc@centOs <chinawraith@163.com>
2022-11-30 18:29:22 +0800

    build

--- lib/server/initialize.d.ts
+++ lib/server/initialize.d.ts
@@ -1,4 +1,5 @@
 /// <reference path="../../src/typings/polyfill.d.ts" />
-import { Context, EntityDict, RowStore } from 'oak-domain/lib/types';
+import { EntityDict } from 'oak-domain/lib/types';
 import { EntityDict as BaseEntityDict } from 'oak-domain/lib/base-app-domain';
-export declare function initialize<ED extends EntityDict & BaseEntityDict, Cxt extends Context<ED>>(path: string, contextBuilder: (scene?: string) => (store: RowStore<ED, Cxt>) => Promise<Cxt>, dropIfExists?: boolean): Promise<void>;
+import { AsyncContext, AsyncRowStore } from 'oak-domain/lib/store/AsyncRowStore';
+export declare function initialize<ED extends EntityDict & BaseEntityDict, Cxt extends AsyncContext<ED>>(path: string, contextBuilder: (scene?: string) => (store: AsyncRowStore<ED, Cxt>) => Promise<Cxt>, dropIfExists?: boolean): Promise<void>;
