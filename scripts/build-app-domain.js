aea833830580b83b17b7ad4d175c59b1f4971644
Xc@centOs <chinawraith@163.com>
2023-04-28 20:01:45 +0800

    新的auth下要对对象之间的路径关系进行消除

--- scripts/build-app-domain.js
+++ scripts/build-app-domain.js
@@ -1,8 +1,13 @@
-
+const { existsSync } = require('fs');
 const {
     buildSchema,
     analyzeEntities,
+    registerIgnoredRelationPathSet,
 } = require(`${process.cwd()}/node_modules/oak-domain/lib/compiler/schemalBuilder`);
+if (existsSync(`${process.cwd()}/src/config/ignoredRelationPathSet.ts`)) {
+    const set = require(`${process.cwd()}/src/config/ignoredRelationPathSet.ts`).default;
+    registerIgnoredRelationPathSet(set);
+}
 
 analyzeEntities(`${process.cwd()}/node_modules/oak-domain/src/entities`, 'oak-domain/lib/entities');
 analyzeEntities(`${process.cwd()}/node_modules/oak-general-business/src/entities`, 'oak-general-business/lib/entities');
