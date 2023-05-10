c3f2792fb031bf1528b29a7515012fabd067be7e
Xc@centOs <chinawraith@163.com>
2023-05-10 19:44:12 +0800

    增加了deduceRelationMap的注入

--- scripts/build-app-domain.js
+++ scripts/build-app-domain.js
@@ -2,14 +2,24 @@ const { existsSync } = require('fs');
 const {
     buildSchema,
     analyzeEntities,
-    registerIgnoredRelationPathSet,
+    registerIgnoredForeignKeyMap,
+    registerIgnoredRelationPathMap,
+    registerDeducedRelationMap,
 } = require(`${process.cwd()}/node_modules/oak-domain/lib/compiler/schemalBuilder`);
-if (existsSync(`${process.cwd()}/src/config/ignoredRelationPathSet.ts`)) {
-    const set = require(`${process.cwd()}/src/config/ignoredRelationPathSet.ts`).default;
-    registerIgnoredRelationPathSet(set);
-}
 
 analyzeEntities(`${process.cwd()}/node_modules/oak-domain/src/entities`, 'oak-domain/lib/entities');
 analyzeEntities(`${process.cwd()}/node_modules/oak-general-business/src/entities`, 'oak-general-business/lib/entities');
 analyzeEntities(`${process.cwd()}/src/entities`);
+if (existsSync(`${process.cwd()}/src/config/relation.ts`)) {
+    const { IgnoredForeignKeyMap, IgnoredRelationPathMap, DeducedRelationMap } = require(`${process.cwd()}/src/config/relation.ts`);
+    if (IgnoredForeignKeyMap) {
+        registerIgnoredForeignKeyMap(IgnoredForeignKeyMap);
+    }
+    if (IgnoredRelationPathMap) {
+        registerIgnoredRelationPathMap(IgnoredRelationPathMap);
+    }
+    if (DeducedRelationMap) {
+        registerDeducedRelationMap(DeducedRelationMap);
+    }
+}
 buildSchema(`${process.cwd()}/src/oak-app-domain`);
