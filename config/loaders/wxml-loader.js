8d0d2c8fb2102fbe10ab426a81d8f2c75f5cb796
Xc <chinawraith@163.com>
2023-08-19 10:05:59 +0800

    笔误了

--- config/loaders/wxml-loader.js
+++ config/loaders/wxml-loader.js
@@ -118,43 +118,48 @@ function codeChunkIncludesT(text) {
  * @param {*} moduleName 
  * @returns 
  */
-function transformCode(text, namespace, moduleName) {
-    const matches = text.match(/{{((\w|\W)*)}}/g);
+function transformCode(text, namespace, moduleName) {    
+    const codeChunkRegex = /{{((\w|\W)*)}}/g;
+    const matches = text.match(codeChunkRegex);
     if (!matches) {
         return text;
     }
 
-    while(matches.length) {
-        const placeholder = matches.shift();
-        
+    let text2 = text;
+    while (matches.length) {
+        const codeChunk = matches.shift();
+        if (codeChunkIncludesT(codeChunk)) {
+            const codeContent = codeChunk.replace(codeChunkRegex, "$1");
+            const ast = parseSync(codeContent);
+            traverse(ast, {
+                enter(path) {
+                    if (path.isCallExpression()) {
+                        const { node } = path;
+                        if (t.isIdentifier(node.callee) && node.callee.name === 't') {
+                            const { arguments } = node;
+                            // 在t的后面加五个参数（oakLocales, oakLng, oakDefaultLng, oakNamespace, oakModule）
+                            arguments.push(
+                                t.identifier('oakLocales'),
+                                t.identifier('oakLng'),
+                                t.identifier('oakDefaultLng'),
+                                t.stringLiteral(namespace),
+                                t.stringLiteral(moduleName)
+                            );
+                            node.callee = t.memberExpression(
+                                t.identifier('i18n'),
+                                t.identifier('t')
+                            );
+                        }
+                    }
+                },
+            });
+            const { code } = transformFromAstSync(ast);
+            assert(code.endsWith(';'));
+            
+            text2 = text2.replace(codeContent, code.slice(0, code.length - 1));
+        }
     }
-
-    const ast = parseSync(match);
-    traverseAst(ast, {
-        enter(path) {
-            if (path.isCallExpression()) {
-                const { node } = path;
-                if (t.isIdentifier(node.callee) && node.callee.name === 't') {
-                    const { arguments } = node;
-                    // 在t的后面加五个参数（oakLocales, oakLng, oakDefaultLng, oakNamespace, oakModule）
-                    arguments.push(
-                        t.identifier('oakLocales'),
-                        t.identifier('oakLng'),
-                        t.identifier('oakDefaultLng'),
-                        t.stringLiteral(namespace),
-                        t.stringLiteral(moduleName)
-                    );
-                    node.callee = t.memberExpression(
-                        t.identifier('i18n'),
-                        t.identifier('t')
-                    );
-                }
-            }
-        },
-    });
-    const { code } = transformFromAstSync(ast);
-    assert(code.endsWith(';'));
-    return `{{${code.substring(0, code.length - 1)}}}`;
+    return text2;
 }
 //////////
 
