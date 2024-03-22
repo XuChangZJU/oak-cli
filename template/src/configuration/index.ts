897b954708a9af4084a9572932419847c5b5dd77
Xc <chinawraith@163.com>
2024-03-22 11:37:35 +0800

    template上的configuration

---
+++ template/src/configuration/index.ts
@@ -0,0 +1,6 @@
+import { AttrUpdateMatrix } from 'oak-domain/lib/types/EntityDesc';
+import { EntityDict } from '@project/oak-app-domain';
+
+export const attrUpdateMatrix: AttrUpdateMatrix<EntityDict> = {
+    
+}
